c6300ba5b02da61aab43f203cf22849d5ba05cba
Jason Viers <bean@beanalby.net>
2013-07-19 22:20:49 -0400

    Added TestForMatch functionality

--- Assets/Scripts/MatchSquare.cs
+++ Assets/Scripts/MatchSquare.cs
@@ -5,13 +5,18 @@ public enum SquareType { None, Robot, Human, Religious, Science, Freedom, Law, S
 
 public class MatchSquare : MonoBehaviour {
 
+    public int posX, posY;
     public SquareType type;
+
+    public Color[] colors;
 
 	// Use this for initialization
 	void Start () {
         System.Array types = System.Enum.GetValues(typeof(SquareType));
         type = (SquareType)types.GetValue(Random.Range(1, types.Length));
         name = type.ToString();
+        Material mat = GetComponentInChildren<MeshRenderer>().material;
+        mat.color = colors[(int)type - 1];
 	}
 	
 	// Update is called once per frame
