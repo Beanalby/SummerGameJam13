3268b16ee57841f47d74d6852bc774daf2fb0483
Jason Viers <bean@beanalby.net>
2013-07-19 22:20:49 -0400

    Added TestForMatch functionality

--- Assets/Scripts/MatchBoard.cs
+++ Assets/Scripts/MatchBoard.cs
@@ -8,6 +8,9 @@ public class MatchBoard : MonoBehaviour {
     const int boardSize = 7;
 
     public MatchSquare[,] squares;
+    private int squareMask;
+
+    MatchSquare current = null;
 
 	// Use this for initialization
 	void Start () {
@@ -19,12 +22,60 @@ public class MatchBoard : MonoBehaviour {
                 GameObject obj = Instantiate(SquarePrefab.gameObject) as GameObject;
                 obj.transform.position = new Vector3(i, j, 0);
                 squares[i,j] = obj.GetComponent<MatchSquare>();
+                squares[i, j].posX = i;
+                squares[i, j].posY = j;
             }
         }
+        squareMask = 1 << LayerMask.NameToLayer("Square");
 	}
 
     // Update is called once per frame
     void Update() {
-	
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, squareMask)) {
+                Debug.Log("I hit " + hit.collider.name + " at " + hit.transform.position);
+                current = hit.collider.GetComponent<MatchSquare>();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            TestForMatch(current);
+        }
 	}
+
+    void ApplyMatch(MatchSquare square1, MatchSquare square2, MatchSquare square3) {
+        Debug.Log("Got Match betwen " +
+            square1.name + " (" + square1.posX + "," + square1.posY + "), " +
+            square2.name + " (" + square2.posX + "," + square2.posY + "), " +
+            square3.name + " (" + square3.posX + "," + square3.posY + ")!");
+
+    }
+
+    void TestForMatch(MatchSquare square) {
+        int x = square.posX;
+        int y = square.posY;
+        Debug.Log("+++ Testing " + square.name + " (" + x + "," + y + ") for matches");
+        if (x > 1)
+            TestSameType(square, squares[x - 2, y], squares[x - 1, y]);
+        if(x > 0 && x < boardSize-1)
+            TestSameType(square, squares[x - 1, y], squares[x + 1, y]);
+        if(x < boardSize-2)
+            TestSameType(square, squares[x + 2, y], squares[x + 1, y]);
+
+        if (y > 1)
+            TestSameType(square, squares[x, y - 2], squares[x, y - 1]);
+        if(y > 0 && y < boardSize-1)
+            TestSameType(square, squares[x, y - 1], squares[x, y + 1]);
+        if(y < boardSize-2)
+            TestSameType(square, squares[x, y + 2], squares[x, y + 1]);
+    }
+
+    bool TestSameType(MatchSquare square1, MatchSquare square2, MatchSquare square3) {
+        if (square1.type == square2.type && square1.type == square3.type) {
+            ApplyMatch(square1, square2, square3);
+            return true;
+        }
+        return false;
+    }
 }
